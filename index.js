503ec8aa8a045ad6ad050a9eb68c9f846a38ab80
Nicole Apono <napononicole@gmail.com>
2024-03-26 23:48:47 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = [
+    'Bobby',
+    'Sammy',
+    'Sally',
+    'Annette',
+    'Sarah',
+    'bobby',
+  ];
+
+  function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(driverObjects, name) {
+    return driverObjects.filter(driver => driver.name === name);
+  }
